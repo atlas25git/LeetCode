19ca002780b91ce4ef8a6741b148c911d743aa6b
atlas25git <atlas.sarthak@gmail.com>
2021-07-28 22:07:28 +0530

    49r

--- 560SubArraySumEqualsK.cpp
+++ 560SubArraySumEqualsK.cpp
@@ -8,9 +8,12 @@ public:
         rsum[0] = 1;
         
         for(auto num : nums)
-        {
+        {   // The idea is that if we can remove a certain 
+            //sum from this running sum, and the value is k, then we
+            //have a subarray whose sum is k.
             sum += num;
             int diff = sum-k;
+            //all the discrete values would be stored
             if(rsum.count(diff))count+=rsum[diff];
             if(rsum.count(sum))rsum[sum]++;
             else rsum[sum]=1;
