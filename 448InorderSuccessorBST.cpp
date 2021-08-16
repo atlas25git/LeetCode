ca325a792cd0a67be91017f75edf8b539d2b3efa
atlas25git <atlas.sarthak@gmail.com>
2021-08-16 18:24:06 +0530

    onwards and upwards

--- 448InorderSuccessorBST.cpp
+++ 448InorderSuccessorBST.cpp
@@ -17,6 +17,42 @@ public:
      * @return: Successor of p.
      */
     TreeNode * inorderSuccessor(TreeNode * root, TreeNode * p) {
+        //there are two condn only, if the given node has right child
+        //then it'll be it's inorder successor, else the root node of 
+        //last greater order
+
+        if(!root || !p)return 0;
+        
+        TreeNode* parent = root;
+        while(p->val != root->val)
+        {
+            if(p->val<root->val)
+            {
+                parent = root;
+                root = root->left;
+            }
+            else root=root->right;
+        }
+
+        if(p->right)
+        {
+            p=p->right;
+            while(p->left)p=p->left;
+            return p;
+        }
+        //no right child
+        else return parent->val>p->val?parent:0;
+    }
+};
+
+class Solution {
+public:
+    /*
+     * @param root: The root of the BST.
+     * @param p: You need find the successor node of p.
+     * @return: Successor of p.
+     */
+    TreeNode * inorderSuccessor(TreeNode * root, TreeNode * p) {
         // write your code here
         if(!root || !p)return 0;
         TreeNode* parent = root;
