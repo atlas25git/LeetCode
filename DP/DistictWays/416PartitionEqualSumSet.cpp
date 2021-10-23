9cb3695f8e34e7801e6e130c9d53d39f555a125e
atlas25git <atlas.sarthak@gmail.com>
2021-10-23 22:24:50 +0530

    dpWays

--- DP/DistictWays/416PartitionEqualSumSet.cpp
+++ DP/DistictWays/416PartitionEqualSumSet.cpp
@@ -1,32 +1,48 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        
-        //the problem could be reduced to simple
-        //ordered set count that'd make up  given sum
-        //if for the given arr, we can discern that sum/2
-        //is present as a descrete set, thus is the negation of total set
-        //a valid sum/2 round up
-        
+    bool canPartition1(vector<int>& nums) {
         int sum=0;
         for(auto x: nums)sum+=x;
-        if(sum%2)return false;
+        if(sum&1)return false;
         
-        vector<vector<bool>>dp(2,vector<bool>(sum+1,0));
+        vector<vector<bool>>dp(2,vector<bool>(sum+1,false));
         
-        //filling up the array
         for(int i=0;i<=nums.size();i++)
             for(int j=0;j<=sum;j++)
-            {
                 if(!j)
                     dp[i%2][j]=true;
                 else if(!i)
                     dp[i%2][j]=false;
                 else if(nums[i-1]<=j)
-                    dp[i%2][j] = dp[(i+1)%2][j]
-                                || dp[(i+1)%2][j-nums[i-1]];
-                else dp[i%2][j] = dp[(i+1)%2][j];
-            }
+                    dp[i%2][j] = dp[(i+1)%2][j] || dp[(i+1)%2][j-nums[i-1]];
+                else 
+                    dp[i%2][j] = dp[(i+1)%2][j];                
+        
         return dp[1][sum/2];
+    }
+    vector<vector<int>>memo;
+    
+    bool canPartition(vector<int>& nums){
+        
+        memo.resize(20001,vector<int>(201,0));
+        int sum=0;
+        for(auto x: nums)sum+=x;
+        if(sum&1)return false;
+        return fill(sum/2,nums,nums.size());
+    }
+    
+    bool fill(int sum,vector<int>& nums,int n){
+        if(sum == 0)
+            return true;
+        if(n<=0 || sum<0)
+            return false;
+        if(memo[sum][n])
+            return memo[sum][n]==2?1:0;
+        bool exc = fill(sum,nums,n-1);
+        bool inc = fill(sum-nums[n-1],nums,n-1);
+        
+        int res = (exc || inc)?2:1;
+        memo[sum][n] = res;
+        return exc || inc;
     }
 };
--- DP/DistictWays/576OutOfBounds.cpp
+++ DP/DistictWays/576OutOfBounds.cpp
@@ -16,3 +16,15 @@ int findPaths(int m, int n, int maxMove, int r, int c) {
 	return dp[r][c][maxMove];
 }
 };
+
+//Just memoize this
+class Solution {
+  public int findPaths(int m, int n, int N, int i, int j) {
+    if (i == m || j == n || i < 0 || j < 0) return 1;
+    if (N == 0) return 0;
+    return findPaths(m, n, N - 1, i - 1, j)
+        + findPaths(m, n, N - 1, i + 1, j)
+        + findPaths(m, n, N - 1, i, j - 1)
+        + findPaths(m, n, N - 1, i, j + 1);
+  }
+}
