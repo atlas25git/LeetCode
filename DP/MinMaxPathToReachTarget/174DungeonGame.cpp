e7f39224bdbd57b38679a95e4fbdf6b242f44827
atlas25git <atlas.sarthak@gmail.com>
2021-11-10 22:40:24 +0530

    dp

--- DP/MinMaxPathToReachTarget/174DungeonGame.cpp
+++ DP/MinMaxPathToReachTarget/174DungeonGame.cpp
@@ -1,64 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+    map<pair<int,int>,int> memo;
+    int calculateMinimumHP(vector<vector<int>>& dg) {
+        //recursive approach:
+        //since we need the answer at 0,0 hence calling the fn 
+        //for 0,0 as initial states.
         
-        dp.resize(dungeon.size(),vector<int>(dungeon[0].size(),INT_MAX));
+        // return rec(dg,0,0);
         
-        return solveBUP1(dungeon);
-    }
-    
-    int solve(vector<vector<int>>& d,int i,int j)
-    {
-        if(i >= d.size() || j >= d[0].size())return INT_MAX;
+        vector<vector<int>>dp (dg.size()+1,vector<int>(dg[0].size()+1,INT_MAX));
         
-        if(i==d.size()-1 && j==d[0].size()-1)
-                return d[i][j]>0?1:-1*(d[i][j])+1;
+        //dp[i][j] will have the min energy required to reach this cell
+        int r = dg.size();
+        int c = dg[0].size();
         
-        if(dp[i][j]!=INT_MAX)
-                return dp[i][j];
-        
-        int r = solve(d,i+1,j);
-        int dw = solve(d,i,j+1);
-        
-        //merge step
-        //for positive power value our king will get those as incentives on
-        //arriving here hence we can reduce this cost from total
-        int mhrc = min(r,dw) - d[i][j];
-        
-        return dp[i][j] = mhrc>0?mhrc:1;
-
-    }
-    
-    int solveBUP(vector<vector<int>>& dg)
-    {
-        int r = dg.size(),c=dg[0].size();
-        vector<vector<int>> dp(r+1,vector<int>(c+1,99));
-        
-        //r-1,c-1 is the destination it's destiation in defined terms
-        //hence should have the minimum possible energy to be calculated
-        dp[r][c-1] = 1;
         dp[r-1][c] = 1;
-        
+        dp[r][c-1] = 1;
         
         for(int i=r-1;i>-1;i--)
             for(int j=c-1;j>-1;j--)
-            {   
-                //since we're iterating from the end thus,
-                //increments in i,j are symbolizing the inherently
-                //previous states.
-                int nd = min(dp[i+1][j],dp[i][j+1]) - dg[i][j];
-                dp[i][j] = nd>0?nd:1;
+            {     
+                    int nd = min(dp[i+1][j],dp[i][j+1]) - dg[i][j];
+                    dp[i][j] = nd>0?nd:1;
             }
-        
-         for(auto x: dp)
-        {
-            for(auto y: x)
-                cout<<y<<"   ";
-            cout<<endl;
-        }
         
         return dp[0][0];
     }
     
+    int rec(vector<vector<int>>& dg,int i,int j)
+    {
+        if(i==dg.size() || j==dg[0].size())
+                return 1e9+7;
+        
+        if(i==dg.size()-1 && j == dg[0].size()-1)
+                return dg[i][j]>0?1:-1*(dg[i][j])+1;
+        
+        if(memo.count({i,j}))
+                return memo[{i,j}];
+        int rw = rec(dg,i+1,j);
+        int dw = rec(dg,i,j+1);
+        
+        int mhrc = min(rw,dw) - dg[i][j];
+        
+        return memo[{i,j}] = mhrc>0?mhrc:1;
+    }
 };
--- DP/MinMaxPathToReachTarget/474ZerosAndOnes.cpp
+++ DP/MinMaxPathToReachTarget/474ZerosAndOnes.cpp
@@ -1,38 +1,9 @@
 class Solution {
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
-        int l = strs.size();
-        vector<vector<vector<int>>> dp(l+1,vector<vector<int>>(m+1,vector<int>(n+1,0)));
-        
-        for(int i=0;i<=l;i++)
-        {   
-            
-            int z = i>0?count(strs[i-1].begin(),strs[i-1].end(),'0'):0;
-            int o = i>0?strs[i-1].size() - z:0;
-           
-            for(int j=0;j<=m;j++){
-                for(int k=0;k<=n;k++)
-                {
-                    if(i==0){
-                        dp[i][j][k] = 0;
-                    }
-                    else if(j>=z && k>=o)
-                    {
-                        dp[i][j][k] = max(dp[i-1][j][k],dp[i-1][j-z][k-o] + 1);
-                    }else
-                    {
-                        dp[i][j][k] = dp[i-1][j][k];
-                    }
-                }
-            }
-        }
-        return dp[l][m][n];
-    }
-};
-
-class Solution {
-public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
+    
+    //Bottom up:
+    
+    int findMaxForm1(vector<string>& strs, int m, int n) {
         //it's a problem on the lines of 0 1 knapsack
         //just that rather than considering just one restraint of wt
         //here we act under the duress of 2 factors m and n
@@ -55,5 +26,40 @@ public:
                 }
         }
         return dp[strs.size()][n][m];
+    }
+    
+     //Recursive + Memoized
+     vector<pair<int,int>> convert(vector<string>& strs)
+     {
+         vector<pair<int,int>> wts;
+         for(auto x: strs)
+         {
+             int z = count(x.begin(),x.end(),'0');
+             int o = x.size() - z;
+             wts.push_back({z,o});
+         }
+         return wts;
+     }
+    vector<vector<vector<int>>> kp;
+     int findMaxForm(vector<string>& strs, int m, int n)
+     {
+         vector<pair<int,int>> wts =convert(strs);
+         kp.resize(strs.size()+1,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
+        
+         return solve(m,n,wts,strs.size());
+     }
+    
+    int solve(int m,int n,vector<pair<int,int>>& wts,int itr)
+    {
+        if((!n && !m) || itr==0)
+            return 0;
+        if(kp[itr][m][n]!=-1)return kp[itr][m][n];
+        if(wts[itr-1].first<=m && wts[itr-1].second<=n)
+                return kp[itr][m][n] = max(
+                    solve(m,n,wts,itr-1),
+                    1+solve(m-wts[itr-1].first,n-wts[itr-1].second,wts,itr-1)
+        );
+        else 
+                return kp[itr][m][n] = solve(m,n,wts,itr-1);
     }
 };
