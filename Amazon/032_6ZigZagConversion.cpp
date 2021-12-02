aff4c261e6aee83f939cb277ce605c6a421a2d05
atlas25git <atlas.sarthak@gmail.com>
2021-12-02 14:44:18 +0530

    amaz

---
+++ Amazon/032_6ZigZagConversion.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows == 1)return s;
+        vector<string> row(min(numRows,(int)s.size()));
+        int cr = 0;
+        int dir = 0;
+        
+        for(auto c: s)
+        {
+            row[cr] += c;
+            if(cr == 0 || cr == row.size()-1)
+                dir++;
+            cr += dir&1?1:-1;
+        }
+        string res;
+        for_each(row.begin(),row.end(),[&res](string s){res+=s;});
+        return res;
+    }
+};
